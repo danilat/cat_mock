1218558279c262c1da0d71721c2a2f431ae7f186
Dani Latorre <dani@danilat.com>
2020-10-14 19:31:41 +0200

    Adding the method post to create a cat

--- __tests__/api_test.js
+++ __tests__/api_test.js
@@ -8,4 +8,16 @@ it('Gets the hello world endpoint', async done => {
     expect(response.status).toBe(200)
     expect(response.body.message).toBe('running')
     done()
-  })
+})
+
+it('Post a new cat', async done => {
+    payload = {name: 'angry cat' ,
+               url: 'https://i.pinimg.com/originals/2f/89/07/2f89076e6091bd0a4d7985a52a3bf504.gif'}
+    const response = await request.post('/cats').send(payload)
+    
+    expect(response.status).toBe(200)
+    expect(response.body.name).toBe(payload['name'])
+    expect(response.body.url).toBe(payload['url'])
+    expect(response.body.howMany).toBe(1)
+    done()
+})
--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-    res.json({message: 'running'})
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, reponse) => {
+    reponse.json({message: 'running'})
 })
+
+app.post('/cats', (request, reponse) => {
+    reponse.json({
+        name: request.body.name,
+        url: request.body.url,
+        howMany: 1
+    })
+})
+
 module.exports = app
